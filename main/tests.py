ec17fbb155faffdb702d1280584c503bbbb74f2f
Gabriella Naomi Hutagalung <gabriella.naomi21@ui.ac.id>
2023-09-12 22:06:49 +0700

    Menambahkan test

--- main/tests.py
+++ main/tests.py
@@ -1,7 +1,6 @@
-from django.test import TestCase
-
 # Create your tests here.
 from django.test import TestCase, Client
+from main.models import Item
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
@@ -11,3 +10,13 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    def setUp(self):
+        Item.objects.create(name = "Bodrex", amount =5, description="Recommended for the treatment of most painful and febrile conditions, for example, headache including migraine, backache, toothache, colds and influenza, sore throat, rheumatic pain and dysmenorrhoea.")
+        Item.objects.create(name = "Neozep Forte", amount =10, description="a medicine to relieve flu symptoms such as fever, headache, nasal congestion and sneezing.")
+
+    def test_items_can_created(self):
+        bodrex = Item.objects.get(name= "Bodrex")
+        neozep_forte = Item.objects.get(name = "Neozep Forte")
+        self.assertEqual(bodrex.amount, 5)
+        self.assertEqual(neozep_forte.name, "Neozep Forte")
